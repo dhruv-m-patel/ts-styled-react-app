cc3aad4b6192847ef5924c0b46538b7c985a6363
Dhruv Patel <dhruv.codelab@gmail.com>
2024-03-17 20:01:16 -0600

    test: Update tests

--- src/common/components/NotFound/NotFound.tsx
+++ src/common/components/NotFound/NotFound.tsx
@@ -4,7 +4,7 @@ import Page from '../Page';
 export default function NotFound() {
   return (
     <Page>
-      <h1>Page not found</h1>
+      <h2>Page not found</h2>
       <p data-testid="message">
         The page you are looking for has been moved or does not exist.
       </p>
