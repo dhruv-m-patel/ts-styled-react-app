0e8a28d355385e57ba215da6eb74ef3c306f7ea7
Dhruv Patel <dhruvpatel.work@gmail.com>
2024-09-30 16:15:57 -0600

    revert: Dont use React.StrictMode - it breaks HMR

--- src/common/router.tsx
+++ src/common/router.tsx
@@ -4,20 +4,18 @@ import loadable from '@loadable/component';
 
 export default function Router() {
   return (
-    <React.StrictMode>
-      <Switch>
-        <Route
-          exact
-          path="/"
-          component={loadable(() => import('./components/HomePage'))}
-        />
-        <Route
-          exact
-          path="/redux"
-          component={loadable(() => import('./components/ReduxExample'))}
-        />
-        <Route component={loadable(() => import('./components/NotFound'))} />
-      </Switch>
-    </React.StrictMode>
+    <Switch>
+      <Route
+        exact
+        path="/"
+        component={loadable(() => import('./components/HomePage'))}
+      />
+      <Route
+        exact
+        path="/redux"
+        component={loadable(() => import('./components/ReduxExample'))}
+      />
+      <Route component={loadable(() => import('./components/NotFound'))} />
+    </Switch>
   );
 }
