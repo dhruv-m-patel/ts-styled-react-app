a9ebd813109ef063829c0afbcc7b7cf5ad8e1fa6
Dhruv Patel <dhruvpatel.work@gmail.com>
2024-09-30 15:32:10 -0600

    feat: Integrate why-did-you-render to debug re-renders

--- src/common/router.tsx
+++ src/common/router.tsx
@@ -4,18 +4,20 @@ import loadable from '@loadable/component';
 
 export default function Router() {
   return (
-    <Switch>
-      <Route
-        exact
-        path="/"
-        component={loadable(() => import('./components/HomePage'))}
-      />
-      <Route
-        exact
-        path="/redux"
-        component={loadable(() => import('./components/ReduxExample'))}
-      />
-      <Route component={loadable(() => import('./components/NotFound'))} />
-    </Switch>
+    <React.StrictMode>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={loadable(() => import('./components/HomePage'))}
+        />
+        <Route
+          exact
+          path="/redux"
+          component={loadable(() => import('./components/ReduxExample'))}
+        />
+        <Route component={loadable(() => import('./components/NotFound'))} />
+      </Switch>
+    </React.StrictMode>
   );
 }
